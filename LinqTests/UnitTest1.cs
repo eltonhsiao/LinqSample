e7fab0249d2fc8e154b3bdb98bc5ee26bba91121
Elton <st10426@gmail.com>
2018-03-24 11:30:47 +0800

    1. practice linq 2. practice Extension method

--- LinqTests/UnitTest1.cs
+++ LinqTests/UnitTest1.cs
@@ -1,7 +1,9 @@
-﻿using ExpectedObjects;
+﻿using System;
+using ExpectedObjects;
 using LinqTests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqTests
 {
@@ -12,11 +14,25 @@ namespace LinqTests
         public void find_products_that_price_between_200_and_500()
         {
             var products = RepositoryFactory.GetProducts();
-            var actual = WithoutLinq.FindProductByPrice(products, 200, 500);
+            var actual = products.FindProduct(p => p.IsTopSaleProduct());
 
             var expected = new List<Product>()
             {
-                new Product{Id=2, Cost=21, Price=210, Supplier="Yahoo" },
+                new Product{Id=3, Cost=31, Price=310, Supplier="Odd-e" },
+                new Product{Id=4, Cost=41, Price=410, Supplier="Odd-e" }
+            };
+
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [TestMethod]
+        public void find_products_that_price_between_200_and_500_with_LINQ()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = products.Where(p => p.Price >= 200 && p.Price <= 500 && p.Cost > 30).ToList();
+
+            var expected = new List<Product>()
+            {
                 new Product{Id=3, Cost=31, Price=310, Supplier="Odd-e" },
                 new Product{Id=4, Cost=41, Price=410, Supplier="Odd-e" }
             };
@@ -26,20 +42,17 @@ namespace LinqTests
     }
 }
 
-internal class WithoutLinq
+internal static class WithoutLinq
 {
-    public static List<Product> FindProductByPrice(IEnumerable<Product> products, int lowBoundary, int highBoundary)
+    public static IEnumerable<Product> FindProduct(this IEnumerable<Product> products, Func<Product, bool> filter)
     {
-        List<Product> productByPrice = new List<Product>();
         foreach (var p in products)
         {
-            if (p.Price >= lowBoundary && p.Price <= highBoundary)
+            if (filter(p))
             {
-                productByPrice.Add(p);
+                yield return p;
             }
         }
-
-        return productByPrice;
     }
 }
 
